e1053d50c2d048e087ca4c1dc3bd92426bed81d4
Alexander <obscureguess@gmail.com>
2020-01-16 19:32:02 +0300

    add ChatUserList layout

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -9,6 +9,9 @@
         <b-col cols="8">
           <chat-message-list />
         </b-col>
+        <b-col cols="2">
+          <chat-user-list />
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -18,13 +21,15 @@
 import ChatNavbar from '../components/ChatNavbar'
 import ChatRoomList from '../components/ChatRoomList'
 import ChatMessageList from '../components/ChatMessageList'
+import ChatUserList from '../components/ChatUserList'
 
 export default {
   name: 'chat',
   components: {
     ChatNavbar,
     ChatRoomList,
-    ChatMessageList
+    ChatMessageList,
+    ChatUserList
   }
 }
 </script>
